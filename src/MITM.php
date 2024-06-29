e20553668779ad55a2ea075e806dab2b8be9c946
PHPrivoxy <balpom@phprivoxy.ru>
2024-06-29 11:56:58 +0000

    Changed MITM.php

--- src/MITM.php
+++ src/MITM.php
@@ -4,19 +4,35 @@ declare(strict_types=1);
 
 namespace PHPrivoxy\Proxy;
 
+use Psr\Http\Server\RequestHandlerInterface;
 use Workerman\Connection\TcpConnection;
 use Workerman\Connection\AsyncTcpConnection;
 use Workerman\Protocols\Http;
 use Workerman\Psr7\ServerRequest;
 use PHPrivoxy\Core\ProxyException;
 use PHPrivoxy\Proxy\MITM\WorkerFactory;
+use PHPrivoxy\Proxy\MITM\ContextProvider;
 use PHPrivoxy\Proxy\MITM\WorkermanResponseDecorator;
 
 class MITM extends PSR15Proxy
 {
+    private ?ContextProvider $contextProvider;
+    private ?string $mitmHost; // MITM worker host.
+
+    public function __construct(
+            RequestHandlerInterface $handler,
+            ?ContextProvider $contextProvider = null,
+            ?string $mitmHost = null
+    )
+    {
+        parent::__construct($handler);
+        $this->contextProvider = $contextProvider;
+        $this->mitmHost = $mitmHost;
+    }
+
     protected function prepare(): void
     {
-        $this->mitmWorkerFactory = new WorkerFactory(); // TODO: that do not initialize again every time (a separate Worker?)
+        $this->mitmWorkerFactory = new WorkerFactory($this->contextProvider, $this->mitmHost); // TODO: that do not initialize again every time (a separate Worker?)
         $this->mitmWorker = $this->mitmWorkerFactory->getWorker($this->host, $this->port);
         $this->mitmWorker->run();
 
