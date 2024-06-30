7678145d2b9ffa34d5801a9dc4427f492bfd4b17
PHPrivoxy <balpom@phprivoxy.ru>
2024-06-30 09:40:35 +0000

    Changed interfaces in MITM.php

--- src/MITM.php
+++ src/MITM.php
@@ -13,38 +13,55 @@ use PHPrivoxy\Core\ProxyException;
 use PHPrivoxy\Proxy\MITM\WorkerFactory;
 use PHPrivoxy\Proxy\MITM\ContextProvider;
 use PHPrivoxy\Proxy\MITM\WorkermanResponseDecorator;
+use \Exception;
 
 class MITM extends PSR15Proxy
 {
     private ?ContextProvider $contextProvider;
+    private ?string $mitmLogDirectory;
     private ?string $mitmHost; // MITM worker host.
+    private WorkerFactory $mitmWorkerFactory;
 
     public function __construct(
             RequestHandlerInterface $handler,
             ?ContextProvider $contextProvider = null,
+            ?string $mitmLogDirectory = null,
             ?string $mitmHost = null
     )
     {
         parent::__construct($handler);
         $this->contextProvider = $contextProvider;
+        $this->mitmLogDirectory = $mitmLogDirectory;
         $this->mitmHost = $mitmHost;
+
+        // TODO: that do not initialize again every time (a separate Worker?)
+        $this->mitmWorkerFactory = new WorkerFactory($this->contextProvider, $this->mitmLogDirectory, null, $this->mitmHost);
     }
 
     protected function prepare(): void
     {
-        $this->mitmWorkerFactory = new WorkerFactory($this->contextProvider, $this->mitmHost); // TODO: that do not initialize again every time (a separate Worker?)
-        $this->mitmWorker = $this->mitmWorkerFactory->getWorker($this->host, $this->port);
-        $this->mitmWorker->run();
+        $this->mitmWorker = null;
+        while (null === $this->mitmWorker) {
+            $this->mitmWorker = $this->mitmWorkerFactory->getWorker($this->host, $this->port);
 
-        Http::requestClass(ServerRequest::class);
-        $this->mitmWorker->onMessage = function (TcpConnection $connection, ServerRequest $request) {
-            $request = $this->sanitizeWorkermanServerRequest($request);
-            $response = $this->handler->handle($request);
-            $response = $response->withoutHeader('Transfer-Encoding');
-            $response = new WorkermanResponseDecorator($response);
-            $connection->send($response);
-            $this->mitmWorker->stop();
-        };
+            Http::requestClass(ServerRequest::class);
+            $this->mitmWorker->onMessage = function (TcpConnection $connection, ServerRequest $request) {
+                $request = $this->sanitizeWorkermanServerRequest($request);
+                $response = $this->handler->handle($request);
+                $response = $response->withoutHeader('Transfer-Encoding');
+                $response = new WorkermanResponseDecorator($response);
+                $connection->send($response);
+                $this->mitmWorker->stop();
+            };
+
+            try {
+                // May be PHP Warning: stream_socket_server(): Unable to connect to tcp://127.0.0.1:xxxxx (Address already in use)
+                @$this->mitmWorker->run();
+            } catch (Exception $exception) {
+                $this->mitmWorker->stop();
+                $this->mitmWorker = null;
+            }
+        }
     }
 
     protected function createRemoteConnection(): AsyncTcpConnection
